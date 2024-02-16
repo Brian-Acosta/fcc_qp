640e6c8df63b24f9b1c55c1a064a2179eb41e3a7
Brian Acosta <bjacosta@seas.upenn.edu>
2024-02-16 15:03:46 -0500

    make plots nice and take results screenshots

--- fcc_qp_test.py
+++ fcc_qp_test.py
@@ -1,47 +1,83 @@
 import os
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 from fcc_qp import FCCQPSolver, FCCQPSolution
 
 
-def load_qp_matrices():
-    data = np.load('test_data/id_qp_log_walking.npz', allow_pickle=True)
+SMALL_SIZE = 20
+MEDIUM_SIZE = 20
+BIGGER_SIZE = 20
+
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
+
+def load_qp_matrices(test_name):
+    data = np.load(f'test_data/id_qp_log_{test_name}.npz', allow_pickle=True)
     return data['qps']
 
 
 def solve(qp, solver):
     solver.Solve(
-        qp['Q'], qp['b'], qp['A_eq'], qp['b_eq'], qp['friction_coeffs'], qp['lb'], qp['ub'])
+        qp['Q'], qp['b'], qp['A_eq'], qp['b_eq'], qp['friction_coeffs'],
+        qp['lb'], qp['ub'])
     result = solver.GetSolution()
     return result
 
 
+@mpl.rc_context({'lines.linewidth': 4})
+def plot_variable(name, val, idx, ylabel, ax):
+    ax.plot(idx, val)
+    ax.set_title(name)
+    ax.set_xlabel('QP #')
+    ax.set_ylabel(ylabel)
+
+
+@mpl.rc_context({'lines.linewidth': 3})
+def make_plots(results):
+    idx = [i for i in range(len(results))]
+    z = np.vstack([result.z for result in results])
+    ts = [result.solve_time for result in results]
+    n = [result.n_iter for result in results]
+
+    # Dimensions of Cassie OSC problem
+    vdot = z[:, :22]
+    u = z[:, 22:32]
+    lambda_h = z[:, 32:38]
+    lambda_c = z[:, 38:]
+
+    fig, ax = plt.subplots(2, 3, figsize=(20, 12))
+
+    plot_variable('U Solution', u, idx, 'u (nm)', ax[0][0])
+    plot_variable('Solve Time', ts, idx, 'Solve Time (seconds)', ax[0][1])
+    plot_variable('Number of Iterations', n, idx, 'Iterations', ax[0][2])
+    plot_variable('Vdot Solution', vdot, idx, 'Vdot Solution', ax[1][0])
+    plot_variable('Lambda_h Solution', lambda_h, idx, 'Lambda_h (N or Nm)',
+                  ax[1][1])
+    plot_variable('Lambda_c Solution', lambda_c, idx, 'lambda_c (N)', ax[1][2])
+    plt.show()
+
+
 def main():
-    qps = load_qp_matrices()
+    qps = load_qp_matrices('running')
+
+    # Dimensions of Cassie OSC problem
     solver = FCCQPSolver(50, 38, 12, 38)
-    solver.set_rho(100)
+    solver.set_rho(1000)
     solver.set_eps(1e-4)
 
-    u_sol = []
-    idxs = []
-    ts = []
+    results = []
     for i in range(len(qps)):
         result = solve(qps[i], solver)
-        u_sol.append(result.z[22:32])
-        idxs.append(i)
-        ts.append(result.solve_time)
+        results.append(result)
 
-    plt.plot(idxs, u_sol)
-    plt.title('Input Solution')
-    plt.xlabel('QP #')
-    plt.ylabel('u (nm)')
-
-    plt.figure()
-    plt.plot(idxs, ts)
-    plt.title('Solve Times')
-    plt.xlabel('QP #')
-    plt.ylabel('Solve Time (s)')
-    plt.show()
+    make_plots(results)
 
 
 if __name__ == '__main__':
